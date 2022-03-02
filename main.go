8df058109518c2fe67266262e3cac00335ef9305
Brodie Peif <brodiep21@hotmail.com>
2022-03-02 14:28:01 -0500

    add writer to create 2 csv files, will join later

--- main.go
+++ main.go
@@ -5,29 +5,47 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
-
+	//file creater for costs
 	file, err := os.Create("costs.csv")
 	if err != nil {
 		log.Fatalf("Could not create file, error is %q", err)
 	}
-	defer file.Close()
+	//file creater for links
+	file2, err := os.Create("links.csv")
+	if err != nil {
+		log.Fatalf("Could not create file, error is %q", err)
+	}
+	//allows writer to write in the correlating file
 	writer := csv.NewWriter(file)
+	writer2 := csv.NewWriter(file2)
+
+	defer file.Close()
+	defer file2.Close()
+
 	defer writer.Flush()
+	defer writer2.Flush()
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.ebay.com"),
 	)
 
+	//finds the link associated with each card, adds it to a slice of strings and prints it to a link file
+	c.OnHTML(".s-item__info.clearfix", func(e *colly.HTMLElement) {
+		link := e.ChildAttr("a", "href")
+		csvLinks := make([]string, 0)
+		csvLinks = append(csvLinks, link)
+		writer2.Write(csvLinks)
+	})
 	//finds the price of each individual card, parses the value to be an int in the format of up to 4 digits with no decimal and prints it to the terminal
 	c.OnHTML(".s-item__detail.s-item__detail--primary", func(e *colly.HTMLElement) {
 		prices := e.ChildText("span.s-item__price")
+		csvCosts := make([]string, 0)
 		prices = strings.ReplaceAll(prices, "$", "")
 		prices = strings.ReplaceAll(prices, ",", "")
 		newprice := ""
@@ -36,20 +54,14 @@ func main() {
 				newprice += string(v)
 			}
 		}
-		csvCosts := make([]string, 0)
 		//filters out the ghost text behind the HTML element indicating $0
 		if newprice == "" {
 		} else {
 			//formats string to int and compares price, then converts back to string in order to create a slice of strings
-			parsePrice, err := strconv.Atoi(newprice)
-			if err == nil && parsePrice <= 1500 {
-				parsePrice2 := strconv.Itoa(parsePrice)
-				csvCosts = append(csvCosts, parsePrice2)
-				writer.Write(csvCosts)
-			}
+			csvCosts = append(csvCosts, newprice)
+			writer.Write(csvCosts)
 		}
 	})
-
 	c.OnRequest(func(request *colly.Request) {
 		fmt.Println("Visiting", request.URL.String()+" and getting video card prices")
 	})
